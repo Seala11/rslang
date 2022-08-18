0e93b116f44ccc8ef3364c3d3fe9889efaae18fc
Natalia Byzova <nata22011997@gmail.com>
2022-08-18 20:52:57 +0300

    refactor: apply prettier

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,39 +4,43 @@ import { NavLink } from 'react-router-dom';
 
 const Menu: React.FC = () => (
   <section className={styles.section__menu}>
-      <h2 className={styles.menu__title}>Изучайте Английский язык в удобном формате</h2>
-      <div className={styles.menu__wrapper}>
-        <NavLink to='/textbook'>
-          <div className={styles.menu__card}>
-            <h3 className={styles.card__title}>Учебник</h3>
-            <div>
-              <img src="assets/images/textbook.png" alt="Textbook" />
-            </div>
-            <p className={styles.card__descr}>Коллекция содержит 3600 часто употребляемых английских слов. 
-            Возможность выбрать подходящий уровень</p>
+    <h2 className={styles.menu__title}>Изучайте Английский язык в удобном формате</h2>
+    <div className={styles.menu__wrapper}>
+      <NavLink to='/textbook'>
+        <div className={styles.menu__card}>
+          <h3 className={styles.card__title}>Учебник</h3>
+          <div>
+            <img src='assets/images/textbook.png' alt='Textbook' />
           </div>
-        </NavLink>
-        <NavLink to='/games'>
-          <div className={styles.menu__card}>
-            <h3 className={styles.card__title}>Игры</h3>
-            <div>
-              <img src="assets/images/games.png" alt="Games" />
-            </div>
-            <p className={styles.card__descr}>Мини-игры &#34;Аудиовызов&#34; и &#34;Спринт&#34;</p>
+          <p className={styles.card__descr}>
+            Коллекция содержит 3600 часто употребляемых английских слов. Возможность выбрать
+            подходящий уровень
+          </p>
+        </div>
+      </NavLink>
+      <NavLink to='/games'>
+        <div className={styles.menu__card}>
+          <h3 className={styles.card__title}>Игры</h3>
+          <div>
+            <img src='assets/images/games.png' alt='Games' />
           </div>
-        </NavLink>
-        <NavLink to='/statistics'>
-          <div className={styles.menu__card}>
-            <h3 className={styles.card__title}>Статистика</h3>
-            <div>
-              <img src="assets/images/statistics.png" alt="Statistics" />
-            </div>
-            <p className={styles.card__descr}>На странице статистики отображается 
-            краткосрочная статистика по мини-играм и по словам за каждый день изучения</p>
+          <p className={styles.card__descr}>Мини-игры &#34;Аудиовызов&#34; и &#34;Спринт&#34;</p>
+        </div>
+      </NavLink>
+      <NavLink to='/statistics'>
+        <div className={styles.menu__card}>
+          <h3 className={styles.card__title}>Статистика</h3>
+          <div>
+            <img src='assets/images/statistics.png' alt='Statistics' />
           </div>
-        </NavLink>
-      </div>
-    </section>
+          <p className={styles.card__descr}>
+            На странице статистики отображается краткосрочная статистика по мини-играм и по словам
+            за каждый день изучения
+          </p>
+        </div>
+      </NavLink>
+    </div>
+  </section>
 );
 
 export default Menu;
--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -3,43 +3,46 @@ import styles from 'src/components/Team/Team.module.scss';
 
 const Team: React.FC = () => (
   <section className={styles.section__team}>
-      <h2 className={styles.team__title}>Наша команда</h2>
-      <div className={styles.team__wrapper}>
-        <div className={styles.member}>
-          <h3 className={styles.member__name}>Аня</h3>
-          <div className={styles.member__img}>
-            <img src="assets/images/anya.png" alt="Anya" />
-          </div>
-          <p className={styles.member__descr}> Разработала игру «Оазис», 
-          в которой необходимо ввести пропущенное в предложении слово. 
-          Придала 3/4 игр внешний вид, сверстала словарь и учебник, 
-          сделала их настройки, а еще графики, компоненты для отображения статистики, 
-          а также страницу авторизации. </p>
+    <h2 className={styles.team__title}>Наша команда</h2>
+    <div className={styles.team__wrapper}>
+      <div className={styles.member}>
+        <h3 className={styles.member__name}>Аня</h3>
+        <div className={styles.member__img}>
+          <img src='assets/images/anya.png' alt='Anya' />
         </div>
-        <div className={styles.member}>
-          <h3 className={styles.member__name}>Наташа</h3>
-          <div className={styles.member__img}>
-            <img src="assets/images/natasha.png" alt="Natasha" />
-          </div>
-          <p className={styles.member__descr}> Разработала игру «Оазис», 
-          в которой необходимо ввести пропущенное в предложении слово. 
-          Придала 3/4 игр внешний вид, сверстала словарь и учебник, 
-          сделала их настройки, а еще графики, компоненты для отображения статистики, 
-          а также страницу авторизации. </p>
-        </div>
-        <div className={styles.member}>
-          <h3 className={styles.member__name}>Марсель</h3>
-          <div className={styles.member__img}>
-            <img src="assets/images/marsel.png" alt="Marsel" />
-          </div>
-          <p className={styles.member__descr}> Разработала игру «Оазис», 
-          в которой необходимо ввести пропущенное в предложении слово. 
-          Придала 3/4 игр внешний вид, сверстала словарь и учебник, 
-          сделала их настройки, а еще графики, компоненты для отображения статистики, 
-          а также страницу авторизации. </p>
-        </div>
+        <p className={styles.member__descr}>
+          {' '}
+          Разработала игру «Оазис», в которой необходимо ввести пропущенное в предложении слово.
+          Придала 3/4 игр внешний вид, сверстала словарь и учебник, сделала их настройки, а еще
+          графики, компоненты для отображения статистики, а также страницу авторизации.{' '}
+        </p>
       </div>
-    </section>
+      <div className={styles.member}>
+        <h3 className={styles.member__name}>Наташа</h3>
+        <div className={styles.member__img}>
+          <img src='assets/images/natasha.png' alt='Natasha' />
+        </div>
+        <p className={styles.member__descr}>
+          {' '}
+          Разработала игру «Оазис», в которой необходимо ввести пропущенное в предложении слово.
+          Придала 3/4 игр внешний вид, сверстала словарь и учебник, сделала их настройки, а еще
+          графики, компоненты для отображения статистики, а также страницу авторизации.{' '}
+        </p>
+      </div>
+      <div className={styles.member}>
+        <h3 className={styles.member__name}>Марсель</h3>
+        <div className={styles.member__img}>
+          <img src='assets/images/marsel.png' alt='Marsel' />
+        </div>
+        <p className={styles.member__descr}>
+          {' '}
+          Разработала игру «Оазис», в которой необходимо ввести пропущенное в предложении слово.
+          Придала 3/4 игр внешний вид, сверстала словарь и учебник, сделала их настройки, а еще
+          графики, компоненты для отображения статистики, а также страницу авторизации.{' '}
+        </p>
+      </div>
+    </div>
+  </section>
 );
 
 export default Team;
