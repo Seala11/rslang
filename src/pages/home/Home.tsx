e0624520f16a3284a971cbc7cd8ad149605bafd2
Natalia Byzova <nata22011997@gmail.com>
2022-08-18 16:15:02 +0300

    feat: add hero section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import styles from 'src/pages/home/Home.module.scss';
 
 const Home: React.FC = () => (
-  <div>
-    <h1 className={`${styles.title}`}>Английский онлайн</h1>
-    <p>
-      Приложение для изучения английского языка в игровой форме. Изучай язык легко с Language Learn
-    </p>
-  </div>
+  <main className={styles.main}>
+    <section className={styles.section__hero}>
+      <div className={styles.hero__info}>
+        <h1 className={styles.hero__title}>Английский онлайн</h1>
+        <p className={styles.hero__text}>Приложение для изучения английского языка 
+        в игровой форме. Изучай язык легко с Language Learn</p>
+      </div>
+      <div>
+        <img src="assets/images/hero.png" alt="Hero" />
+      </div>
+    </section>
+  </main>
 );
 
 export default Home;
