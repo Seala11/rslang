ab29455cb7668093ccb4b7dace09f5a24c33ca9f
Natalia Byzova <nata22011997@gmail.com>
2022-08-18 21:18:32 +0300

    refactor: add data from developers

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import styles from 'src/components/Footer/Footer.module.scss';
+import developers from 'src/data/developers';
 
 const Footer: React.FC = () => (
   <footer className={styles.footer}>
     <div className={styles.footer__wrapper}>
       <ul className={styles.footer__gitList}>
         <li className={styles.footer__gitItem}>
-          <a className={styles.footer__gitLink} href='https://github.com/Seala11'>
-            Seala11
+          <a className={styles.footer__gitLink} href={`${developers[0].githubLink}`}>
+            {developers[0].githubName}
           </a>
         </li>
         <li className={styles.footer__gitItem}>
-          <a className={styles.footer__gitLink} href='https://github.com/Verbena336'>
-            Verbena336
+          <a className={styles.footer__gitLink} href={`${developers[1].githubLink}`}>
+          {developers[1].githubName}
           </a>
         </li>
         <li className={styles.footer__gitItem}>
-          <a className={styles.footer__gitLink} href='https://github.com/iamrealmarsel'>
-            iamrealmarsel
+          <a className={styles.footer__gitLink} href={`${developers[2].githubLink}`}>
+          {developers[2].githubName}
           </a>
         </li>
       </ul>
--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -1,44 +1,36 @@
 import React from 'react';
 import styles from 'src/components/Team/Team.module.scss';
+import developers from 'src/data/developers';
 
 const Team: React.FC = () => (
   <section className={styles.section__team}>
     <h2 className={styles.team__title}>Наша команда</h2>
     <div className={styles.team__wrapper}>
       <div className={styles.member}>
-        <h3 className={styles.member__name}>Аня</h3>
+        <h3 className={styles.member__name}>{developers[0].name}</h3>
         <div className={styles.member__img}>
-          <img src='assets/images/anya.png' alt='Anya' />
+          <img src={`${developers[0].srcImg}`} alt={`${developers[0].altImg}`} />
         </div>
         <p className={styles.member__descr}>
-          {' '}
-          Разработала игру «Оазис», в которой необходимо ввести пропущенное в предложении слово.
-          Придала 3/4 игр внешний вид, сверстала словарь и учебник, сделала их настройки, а еще
-          графики, компоненты для отображения статистики, а также страницу авторизации.{' '}
+        {developers[0].tasks}
         </p>
       </div>
       <div className={styles.member}>
-        <h3 className={styles.member__name}>Наташа</h3>
+        <h3 className={styles.member__name}>{developers[1].name}</h3>
         <div className={styles.member__img}>
-          <img src='assets/images/natasha.png' alt='Natasha' />
+          <img src={`${developers[1].srcImg}`} alt={`${developers[1].altImg}`} />
         </div>
         <p className={styles.member__descr}>
-          {' '}
-          Разработала игру «Оазис», в которой необходимо ввести пропущенное в предложении слово.
-          Придала 3/4 игр внешний вид, сверстала словарь и учебник, сделала их настройки, а еще
-          графики, компоненты для отображения статистики, а также страницу авторизации.{' '}
+        {developers[1].tasks}
         </p>
       </div>
       <div className={styles.member}>
-        <h3 className={styles.member__name}>Марсель</h3>
+        <h3 className={styles.member__name}>{developers[2].name}</h3>
         <div className={styles.member__img}>
-          <img src='assets/images/marsel.png' alt='Marsel' />
+          <img src={`${developers[2].srcImg}`} alt={`${developers[2].altImg}`} />
         </div>
         <p className={styles.member__descr}>
-          {' '}
-          Разработала игру «Оазис», в которой необходимо ввести пропущенное в предложении слово.
-          Придала 3/4 игр внешний вид, сверстала словарь и учебник, сделала их настройки, а еще
-          графики, компоненты для отображения статистики, а также страницу авторизации.{' '}
+        {developers[2].tasks}
         </p>
       </div>
     </div>
