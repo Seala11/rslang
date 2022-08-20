4f6b5ed45590cfffd85390f72100297452a0c4d5
mars <maspam@yandex.ru>
2022-08-21 02:07:01 +0300

    feat: add word details component

---
+++ src/section/word-list/WordDetails/WordDetails.tsx
@@ -0,0 +1,129 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { UrlPath } from 'src/requests/interfaceAPI';
+import styles from './WordDetails.module.scss';
+import { IWordDetailsProps } from './IWordDetailsProps';
+
+let audioCounter = 0;
+let player: HTMLAudioElement;
+
+const WordDetails: React.FC<IWordDetailsProps> = ({ word }) => {
+  const [stop, setStop] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+
+  console.log('раз');
+
+  const playAudio = useCallback(async () => {
+    const audioSources = [
+      `${UrlPath.base}/${word?.audio}`,
+      `${UrlPath.base}/${word?.audioMeaning}`,
+      `${UrlPath.base}/${word?.audioExample}`,
+    ];
+
+    if (audioCounter >= audioSources.length) {
+      audioCounter = 0;
+      return false;
+    }
+
+    player.src = audioSources[audioCounter];
+    await player.play();
+    audioCounter += 1;
+
+    return true;
+  }, [word]);
+
+  const stopAudio = () => {
+    player.pause();
+    audioCounter = 0;
+  };
+
+  useEffect(() => {
+    player = new Audio();
+
+    const playerHandler = () => {
+      playAudio();
+    };
+
+    player.addEventListener('ended', playerHandler);
+
+    return () => {
+      player.removeEventListener('ended', playerHandler);
+      stopAudio();
+      setStop(false);
+      setDisabled(false);
+    };
+  }, [playAudio, word]);
+
+  const playClickHandler = async () => {
+    setDisabled(true);
+    await playAudio();
+    setStop(true);
+  };
+
+  const stopClickHandler = () => {
+    setDisabled(false);
+    stopAudio();
+    setStop(false);
+  };
+
+  return word ? (
+    <div className={styles.details}>
+      <div
+        className={styles.img}
+        style={{ backgroundImage: `url(${UrlPath.base}/${word.image})` }}
+      />
+      <div className={styles.content}>
+        <span className={styles.word}>{word.word}</span>
+        <span className={styles.wordTranslate}>{word.wordTranslate}</span>
+        <div className={styles.audioWrapper}>
+          <span className={styles.transcription}>{word.transcription}</span>
+          <div className={styles.audio}>
+            {stop ? (
+              <button className={styles.btn} type='button' onClick={stopClickHandler}>
+                <svg
+                  focusable='false'
+                  aria-hidden='true'
+                  viewBox='0 0 24 24'
+                  data-testid='StopIcon'
+                  fill='gray'
+                >
+                  <path d='M6 6h12v12H6z' />
+                </svg>
+              </button>
+            ) : (
+              <button
+                className={styles.btn}
+                type='button'
+                onClick={playClickHandler}
+                disabled={disabled}
+              >
+                <svg
+                  focusable='false'
+                  aria-hidden='true'
+                  viewBox='0 0 24 24'
+                  data-testid='VolumeUpIcon'
+                  fill='gray'
+                >
+                  <path d='M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z' />
+                </svg>
+              </button>
+            )}
+          </div>
+        </div>
+        <div className={styles.description}>
+          <h4 className={styles.caption}>Значение</h4>
+          <p className={styles.text} dangerouslySetInnerHTML={{ __html: word.textMeaning }} />
+          <p className={styles.textTranslate}>{word.textMeaningTranslate}</p>
+        </div>
+        <div className={styles.description}>
+          <h4 className={styles.caption}>Пример</h4>
+          <p className={styles.text} dangerouslySetInnerHTML={{ __html: word.textExample }} />
+          <p className={styles.textTranslate}>{word.textExampleTranslate}</p>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div>LOADING...</div>
+  );
+};
+
+export default WordDetails;
