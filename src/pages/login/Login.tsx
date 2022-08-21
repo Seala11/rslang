c86252253fb04baa9cef139aa3e7efb01c6a7c7f
Hanna Papova <seal.annuska@gmail.com>
2022-08-21 18:24:26 -0400

    feat: add form validation

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,47 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from 'src/pages/login/Login.module.scss';
-// import getAllAggrWordsAPI from 'src/requests/aggregatedWords/getAllAggrWordsAPI';
-// import getStatisticAPI from 'src/requests/statistics/getStatisticsAPI';
-// import createUserAPI from 'src/requests/users/createUserAPI';
-// import getSettingsAPI from 'src/requests/userSettings/getSettingsAPI';
+import ErrorMessages from './constantsLogin';
 
-const Login: React.FC = () => {
-  // createUserAPI({ name: 'hanpa', email: 'tryaga', password: '123' });
-  // createUserAPI({ name: 'hanpttttta', email: 'tryaga@ggggg', password: '123886868768' });
-  // createUserAPI({ name: 'hanpa', email: 'tryaga@gmail.com', password: '123uigjhhjjn' });
-  // getAllAggrWordsAPI('666ll');
-  // getStatisticAPI('62fd8d4d739f660016187d52');
-  // getSettingsAPI('62fd8d4d739f660016187d52');
-  // eslint-disable-next-line no-console
-  console.log('stop');
+const Login = () => {
+  const [values, setValues] = useState({ name: '', password: '', email: '' });
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const nameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, name: event.target.value });
+  };
+
+  const passwordHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, password: event.target.value });
+    if (passwordError) setPasswordError(false);
+  };
+
+  const emailHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, email: event.target.value });
+  };
+
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(values);
+
+    if (values.password.trim().length < 8) setPasswordError(true);
+  };
 
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>Регистрация</h1>
       <p className={styles.subtitle}>Используй все возможности приложения!</p>
 
-      <form action='submit' className={styles.form}>
+      <form action='submit' className={styles.form} onSubmit={handleSubmit}>
         <label htmlFor='name' className={styles.label}>
           Имя
-          <input id='name' type='text' className={styles.input} placeholder='Вася' />
+          <input
+            name='name'
+            id='name'
+            type='text'
+            className={styles.input}
+            placeholder='Вася'
+            onChange={nameHandler}
+          />
         </label>
+        <small className={styles.error}>Error</small>
+
         <label htmlFor='email' className={styles.label}>
           Почта
-          <input id='email' type='email' className={styles.input} placeholder='example@gmail.com' />
+          <input
+            name='email'
+            id='email'
+            type='email'
+            className={styles.input}
+            placeholder='example@gmail.com'
+            onChange={emailHandler}
+          />
         </label>
-        <label htmlFor='password' className={styles.label}>
+        <small className={styles.error}>Error</small>
+
+        <label htmlFor='password' className={`${styles.label} ${styles.label_password}`}>
           Пароль
-          <input id='password' type='password' className={styles.input} placeholder='Не менее 8 символов' />
+          <input
+            name='password'
+            id='password'
+            type={passwordShown ? 'text' : 'password'}
+            className={styles.input}
+            placeholder='Не менее 8 символов'
+            onChange={passwordHandler}
+          />
+          <button
+            type='button'
+            aria-label='Show password'
+            onClick={togglePassword}
+            className={`${styles.password} ${
+              passwordShown ? styles.password_hide : styles.password_show
+            }`}
+          />
         </label>
+
+        <small className={styles.error}>{passwordError ? ErrorMessages.PASSWORD : ''}</small>
 
         <button className={styles.button} type='submit'>
           Зарегистрироваться
         </button>
       </form>
+
       <p className={styles.subtitle}>
         Уже есть аккаунт?
         <a className={styles.link} href='./'>
-          Войти в учетную запись
+          Войти в учётную запись
         </a>
       </p>
     </div>
---
+++ src/pages/login/constantsLogin.ts
@@ -0,0 +1,7 @@
+enum ErrorMessages {
+  NAME = 'Введите имя',
+  PASSWORD = 'Пароль должен содержать больше 8 символов',
+  EMAIL = 'Введите коректный адрес почты',
+}
+
+export default ErrorMessages;
