3cd6f8d6658b3d3ce774bfb85cf2adcbb5dc420b
Natalia Byzova <nata22011997@gmail.com>
2022-08-22 19:13:20 +0300

    feat: add games page

--- src/pages/games/Games.tsx
+++ src/pages/games/Games.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from 'src/pages/games/Games.module.scss';
+import MenuCard from 'src/components/MenuCard';
+import GAMES_LIST from 'src/data/games';
 
 const Games: React.FC = () => (
-  <div>
-    <h1 className={`${styles.title}`}>Игры</h1>
-    <p>Аудиовызов</p>
-    <p>Спринт</p>
+  <main className='main'>
+    <div className={styles.wrapper}>
+    <h2 className={styles.title}>Игры</h2>
+    <div className={styles.cards}>
+      {GAMES_LIST.map((item) => {
+        const { title, path, altImg, srcImg, text } = item;
+        return <MenuCard key={item.id} {...{ title, path, altImg, srcImg, text }} />;
+      })}
+    </div>
   </div>
+  </main>
 );
 
 export default Games;
