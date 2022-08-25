64d7b533635665a2ae406c48667e055921e08e06
Hanna Papova <seal.annuska@gmail.com>
2022-08-25 19:46:35 -0400

    feat: disable diff button if word id added

--- src/containers/word-list/WordButtons/WordButtons.tsx
+++ src/containers/word-list/WordButtons/WordButtons.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getUserId, getUserToken } from 'src/helpers/storage';
-import { useAppDispatch } from 'src/store/hooks';
-import { fetchCreateDiffWord } from 'src/store/userWordsSlice';
+import { useAppDispatch, useAppSelector } from 'src/store/hooks';
+import { fetchCreateDiffWord, getDifficultWords } from 'src/store/userWordsSlice';
 import { IWordButtonsProps } from './IWordButtonsProps';
 import styles from './WordButtons.module.scss';
 
 const WordButtons: React.FC<IWordButtonsProps> = ({ word }) => {
   const dispatch = useAppDispatch();
+  const userDiffWords = useAppSelector(getDifficultWords);
+
+  const [diffWord, setDiffWord] = useState(false);
+
+  const checkWordIsDiff = () => {
+    const wordIsAdded = userDiffWords.some((userWord) => userWord.id === word?.id);
+    if (wordIsAdded) setDiffWord(() => true);
+  };
 
   const addDiffWord = () => {
     console.log('add to diff', word);
     dispatch(fetchCreateDiffWord(getUserId(), word?.id, `${word?.group}`, getUserToken()));
+    checkWordIsDiff();
   };
 
   const addLearnedWord = () => {
     console.log('add to leafned', word);
   };
 
+  useEffect(() => {
+    console.log(word);
+    console.log(userDiffWords);
+    const wordIsAdded = userDiffWords.some((userWord) => userWord.id === word?.id);
+    console.log(wordIsAdded);
+    if (wordIsAdded) setDiffWord(() => true);
+    if (!wordIsAdded) setDiffWord(() => false);
+  }, [userDiffWords, setDiffWord, word, diffWord]);
+
   return (
     <div className={styles.wordButtons}>
-      <button type='button' onClick={addDiffWord} className={styles.wordButton}>
+      <button type='button' onClick={addDiffWord} className={styles.wordButton} disabled={diffWord}>
         Сложное слово
       </button>
       <button type='button' onClick={addLearnedWord} className={styles.wordButton}>
