5bfab77f749ce56e47bc3e6d307ebcb8a4c050a5
Hanna Papova <seal.annuska@gmail.com>
2022-09-04 13:50:41 -0300

    feat: add style

--- src/containers/DifficultWords/DifficultWord/DifficultWord.tsx
+++ src/containers/DifficultWords/DifficultWord/DifficultWord.tsx
@@ -50,7 +50,7 @@ const DifficultWord: React.FC<IDifficultWordProps> = ({ word, audio }) => {
   };
 
   const stopClickHandler = () => {
-    player.pause()
+    player.pause();
     dispatch(removeAudioPlay());
   };
 
@@ -77,43 +77,56 @@ const DifficultWord: React.FC<IDifficultWordProps> = ({ word, audio }) => {
         style={{ backgroundImage: `url(${UrlPath.BASE}/${word?.image})` }}
       />
       <div className={styles.content}>
-        <div className={styles.wordDetails}>
-          <span className={styles.word}>{word?.word}</span>
-          <span className={styles.wordTranslate}>{word?.wordTranslate}</span>
-          <div className={styles.audioWrapper}>
-            <span className={styles.transcription}>{word?.transcription}</span>
-            {audioPlay === word?._id ? (
-              <button className={styles.btn} type='button' onClick={stopClickHandler}>
-                <svg
-                  focusable='false'
-                  aria-hidden='true'
-                  width='24px'
-                  height='24px'
-                  viewBox='0 0 24 24'
-                  data-testid='StopIcon'
-                  fill='gray'
-                >
-                  <path d='M6 6h12v12H6z' />
-                </svg>
-              </button>
-            ) : (
-              <button className={styles.btn} type='button' onClick={playClickHandler}>
-                <svg
-                  focusable='false'
-                  aria-hidden='true'
-                  width='24px'
-                  height='24px'
-                  viewBox='0 0 24 24'
-                  data-testid='VolumeUpIcon'
-                  fill='gray'
-                >
-                  <path d='M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z' />
-                </svg>
-              </button>
-            )}
+        <div className={styles.details_wrapper}>
+          <div className={styles.wordDetails}>
+            <span className={styles.word}>{word?.word}</span>
+            <div className={styles.audioWrapper}>
+              <span className={styles.transcription}>{word?.transcription}</span>
+              {audioPlay === word?._id ? (
+                <button className={styles.btn} type='button' onClick={stopClickHandler}>
+                  <svg
+                    focusable='false'
+                    aria-hidden='true'
+                    width='24px'
+                    height='24px'
+                    viewBox='0 0 24 24'
+                    data-testid='StopIcon'
+                    fill='gray'
+                  >
+                    <path d='M6 6h12v12H6z' />
+                  </svg>
+                </button>
+              ) : (
+                <button className={styles.btn} type='button' onClick={playClickHandler}>
+                  <svg
+                    focusable='false'
+                    aria-hidden='true'
+                    width='24px'
+                    height='24px'
+                    viewBox='0 0 24 24'
+                    data-testid='VolumeUpIcon'
+                    fill='gray'
+                  >
+                    <path d='M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z' />
+                  </svg>
+                </button>
+              )}
+            </div>
           </div>
+
+          <button
+            type='button'
+            onClick={removeWord}
+            className={styles.button}
+            disabled={disableButton}
+          >
+            Удалить из списка
+          </button>
         </div>
-        <div>
+
+        <span className={styles.wordTranslate}>{word?.wordTranslate}</span>
+
+        <div className={styles.examples_wrapper}>
           <h5 className={styles.descrTitle}>Значение</h5>
           <p
             className={styles.descrText}
@@ -133,10 +146,8 @@ const DifficultWord: React.FC<IDifficultWordProps> = ({ word, audio }) => {
             dangerouslySetInnerHTML={{ __html: word?.textExampleTranslate || '' }}
           />
         </div>
+
       </div>
-      <button type='button' onClick={removeWord} className={styles.button} disabled={disableButton}>
-        Удалить из списка
-      </button>
     </div>
   );
 };
